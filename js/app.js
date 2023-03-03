f58ce8facbb6e35541ee855c0722b0d31a227bb3
Martin Barron <martinba00@hotmail.com>
2023-03-02 22:50:17 -0600

    primer avance

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const cardsMusic = document.getElementById('cards')
+const discoArtist = document.getElementById('disco').content
+const fragment = document.createDocumentFragment()
+let topMusic = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadMusic()
+})
+
+const loadMusic = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '31ade94c35msh40229da876aeb6dp132d74jsn7d5b1c15ff2e',
+            'X-RapidAPI-Host': 'ip-geo-location4.p.rapidapi.com'
+        }
+    };
+    
+    fetch('https://ip-geo-location4.p.rapidapi.com/?ip=8.8.8.8', options)
+        .then(response => response.json())
+        .then(response => {
+            topMusic = response
+            crearTarjetas()
+        })
+        .catch(err => console.error(err));
+}
+
+const crearTarjetas = () => {
+    topMusic.forEach((artista) => {
+        discoArtist.querySelector('img')
+    })
+}
